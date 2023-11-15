3d68157047df56083a3bf8812879e6aa10517cf2
PRATHAM MEHTANI <mehtani2020@gmail.com>
2023-11-15 16:53:08 +0530

    user download api csv

--- server/routes/users.js
+++ server/routes/users.js
@@ -26,27 +26,15 @@ router.post('/createSession',passport.authenticate(
 // //get logged in user
 router.get('/loggedin_user',userController.getLoggedInUser);
 
-// //get worker details
-// router.post('/getworker',userController.getWorkerbyId);
-// // router.get('/getmanager',userController.getManagerbyId);
-// router.post('/get_allocated_children',userController.getAllocatedChildren)
-
 router.post('/image_upload',upload.single('file'),userController.imageUpload);
 router.post('/get_image',userController.getImage);
 router.post('/delete_image',userController.deleteImage);
-
-// router.post('/get_messages',userController.getMessages);
-// router.post('/messagebyid',userController.getMessagebyId);
-// router.post('/all_seen',userController.markAllSeen);
-// router.post('/all_delete',userController.deleteAllMsg);
-// router.post('/getunseenmsgs',userController.getUnSeenMessages)
-
 
 router.post('/sendmail',userController.sendmail);
 
 router.post('/reset_password',userController.sendResetMail);
 
-// router.get('/download_csv',userController.csvDownload);
+router.get('/download_csv',userController.csvDownload);
 
 // router.post('/createsessionjwt',authentication.createSession);
 module.exports=router;
