0881ea4914f058e881d1e8a375878f522456eecb
PRATHAM MEHTANI <mehtani2020@gmail.com>
2023-12-02 23:35:12 +0530

    config

--- ml_model/flask_app.py
+++ ml_model/flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import pickle
-
+import os
 app = Flask(__name__)
 
 # Load your trained model
@@ -63,5 +63,6 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
 
